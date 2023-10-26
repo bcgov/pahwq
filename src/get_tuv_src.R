8123c71099b05c1486eb5af8378b48455c3a2912
Andy Teucher <andy.teucher@gmail.com>
2023-10-26 11:34:23 -0700

    Create Makefile.win, don't need tuvlog.txt

--- src/get_tuv_src.R
+++ src/get_tuv_src.R
@@ -5,6 +5,7 @@ dir <- "~/dev/TUV/V5.4"
 files <- list.files(dir)
 dirs <- list.dirs(dir, recursive = FALSE, full.names = FALSE)
 
-files <- setdiff(files, c(dirs, "tuv", "tuv.exe"))
+files <- setdiff(files, c(dirs, "tuv", "tuv.exe", "tuvlog.txt"))
 
 file.copy(file.path(dir, files), "src", overwrite = TRUE)
+file.copy("src/Makefile", "src/Makefile.win", overwrite = TRUE)
