aba1e09ee61e3f0f4aa1fa4bd8006c76977d4934
Andy Teucher <andy.teucher@gmail.com>
2023-10-30 11:04:43 -0700

    Test the whole workflow and numeric accuracy

--- tests/testthat/test-ptlm.R
+++ tests/testthat/test-ptlm.R
@@ -14,4 +14,20 @@ test_that("plc_50 works", {
   expect_equal(plc_50(590, 450), 14.68, tolerance = 0.01)
 })
 
+test_that("The whole shebang works", {
+  local_tuv_dir()
+  setup_tuv_options(
+    depth_m = 0.25,
+    lat = 49.601632,
+    lon = -119.605862,
+    elev_km = 0.342,
+    DOC = 5,
+    date = "2023-06-21"
+  )
+  tuv(quiet = TRUE)
+  res <- get_tuv_results(file = "out_irrad_y")
+  expect_equal(pabs <- p_abs(res, "Anthracene"), 430.86, tolerance = 0.01)
+  expect_equal(plc_50(pabs, NLC50 = 450), 16.71, tolerance = 0.01)
+})
+
 # TODO: test Pabs
