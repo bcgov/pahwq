84e2250a0107e3f7bdebae1099ab7be38555c47e
Andy Teucher <andy.teucher@gmail.com>
2023-10-31 10:18:12 -0700

    Use state inspector to ensure tests are clean

---
+++ tests/testthat/helper-state.R
@@ -0,0 +1,7 @@
+set_state_inspector(function() {
+  list(
+    options = options(),
+    envvars = Sys.getenv(),
+    files = list.files(tools::R_user_dir("pahwq", "data"), recursive = TRUE)
+  )
+})
