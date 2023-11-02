3254c3bc874a4fcc8d648ffa5bd6a32baedbf4ef
Andy Teucher <andy.teucher@gmail.com>
2023-11-02 11:09:45 -0700

    Use rounding instead of tolerance for numeric expectations

--- tests/testthat/test-kd-calc.R
+++ tests/testthat/test-kd-calc.R
@@ -11,13 +11,13 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 test_that("kd_305 works at extremes of DOC", {
-  expect_equal(kd_305(0.2), 0.51, tolerance = 0.01)
-  expect_equal(kd_305(10), 47.00, tolerance = 0.01)
-  expect_equal(kd_305(23), 130.70, tolerance = 0.01)
+  expect_equal(round(kd_305(0.2), 2), 0.51)
+  expect_equal(round(kd_305(10), 2), 47.00)
+  expect_equal(round(kd_305(23), 2), 130.70)
 })
 
 test_that("kd_lambda works at extremes of wavelengths", {
-  expect_equal(kd_lambda(10, 280), c("280" = 73.71), tolerance = 0.01)
-  expect_equal(kd_lambda(10, 305), c("305" = 47.00), tolerance = 0.01)
-  expect_equal(kd_lambda(10, 400), c("400" = 8.501), tolerance = 0.01)
+  expect_equal(round(kd_lambda(10, 280), 2), c("280" = 73.71))
+  expect_equal(round(kd_lambda(10, 305), 2), c("305" = 47.00))
+  expect_equal(round(kd_lambda(10, 400), 2), c("400" = 8.50))
 })
