460f44f989903c58b50de57196b60263fe5ec16b
Andy Teucher <andy.teucher@gmail.com>
2023-11-01 17:48:08 -0700

    add both datasets to internal data

--- data-raw/o3.R
+++ data-raw/o3.R
@@ -1,5 +1,3 @@
-## code to prepare `o3` dataset goes here
-
 lines <- readLines("inst/tuv_data/DATAE1/ATM/o3column.dat")
 datalines <- lines[5:27][seq(1, 23, 2)]
 lst <- strsplit(datalines, "\\s+")
@@ -10,5 +8,3 @@ o3$month <- month.abb
 
 # find the latitude interval (i.e., the column) with:
 # findInterval(-86, seq(-85, +85, length.out = 18), all.inside = TRUE)
-
-usethis::use_data(o3, internal = TRUE, overwrite = TRUE)
