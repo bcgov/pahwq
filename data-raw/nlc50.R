618c4f853ebe053053b0dfc75c51959274cdc193
Andy Teucher <andy.teucher@gmail.com>
2023-11-28 16:42:57 -0800

    Add reference for McGrath et al 2018 for NLC50 data

--- data-raw/nlc50.R
+++ data-raw/nlc50.R
@@ -10,6 +10,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+## Supplemental data from:
+# McGrath, J.A., Fanelli, C.J., Di Toro, D.M., Parkerton, T.F., Redman, A.D.,
+# Paumen, M.L., Comber, M., Eadsforth, C.V. and den Haan, K. (2018),
+# Re-evaluation of target lipid model–derived HC5 predictions for hydrocarbons.
+# Environ Toxicol Chem, 37: 1579-1593. https://doi.org/10.1002/etc.4100
+
 library(readr)
 
 nlc50 <- read_csv("data-raw/nlc50_mcgrath_2018.csv")
