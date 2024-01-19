f626284432e6157f246a137a307f9d4f62fea4a1
Andy Teucher <andy.teucher@gmail.com>
2024-01-18 16:42:57 -0800

    Export surrogate spectra sensitivity results

--- inst/sens-vals-from-vignette.R
+++ inst/sens-vals-from-vignette.R
@@ -25,4 +25,24 @@ sens_analysis_results <- map(test_list, \(x) {
 }) |>
   bind_rows()
 
-write_csv(sens_analysis_results, glue("inst/{Sys.Date()}_pahwq-sensitivity-analysis-results.csv"))
+write_csv(
+  sens_analysis_results,
+  glue("inst/{Sys.Date()}_pahwq-sensitivity-analysis-results.csv")
+)
+
+surrogate_test_list <- ls(pattern = "plc50_df_")
+
+surrogate_sens_analysis_results <- map(surrogate_test_list, \(x) {
+  df <- get(x) |>
+    select(-starts_with("doc"), -tuv_res, -timing, -plc_nlc_ratio) |>
+    filter(grepl("[0-9]", PAH) | abs_spectra == "specific")
+
+  df |>
+    relocate(nlc50, .after = PAH)
+}) |>
+  bind_rows()
+
+write_csv(
+  surrogate_sens_analysis_results,
+  glue("inst/{Sys.Date()}_pahwq-alk-surrogate-spectra-analysis-results.csv")
+)
