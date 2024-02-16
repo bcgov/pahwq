2d2fb5ec292dfa37e7d5432525230400bfd6a07b
Andy Teucher <andy.teucher@gmail.com>
2024-02-16 15:28:10 -0800

    Sensitivity function for DOC, depth, time of year

---
+++ R/sensitivity-kd-depth.R
@@ -0,0 +1,122 @@
+#' Sensitivity analysis for DOC, depth, and time of year
+#'
+#' Enter a single location and PAH, and a range of values for `DOC` or `Kd_ref`,
+#' water depth, and months and get back a data.frame of NLC50, Pabs, and PLC50
+#' values.
+#'
+#' You can add other variables beyond those listed explicitly, but if there are
+#' too many combinations it will create many runs of the TUV model, which can
+#' take a long time. Additionally, explicit variable checking is only performed
+#' on `pah`, `lat`, `lon`, `elev_m`, `DOC`, `Kd_ref`, `depth`, and `months`.
+#' Passing invalid values of other parameters may cause cryptic errors or
+#' unexpected results
+#'
+#' @inheritParams tuv
+#' @inheritParams plc50
+#' @param months a vector of months as integers
+#' @param ... other parameters passed on to the tuv model. See [tuv()]
+#'
+#' @return A data.frame of all of the input parameters, plus a list column
+#'   containing TUV results and NLC50, Pabs, and PLC50 values
+#' @export
+#'
+#' @examples
+#' sens_kd_depth(
+#'   "Anthracene",
+#'   lat = 52,
+#'   lon = -113,
+#'   Kd_ref = 40:45,
+#'   depth_m = c(0.25, 0.5, 1),
+#'   months = 6:7
+#' )
+sens_kd_depth <- function(pah = NULL,
+                          lat = NULL,
+                          lon = NULL,
+                          elev_m = NULL,
+                          DOC = NULL,
+                          Kd_ref = NULL,
+                          depth_m = NULL,
+                          months = NULL, ...) {
+
+  stopifnot(is.character(pah) && length(pah) == 1)
+  pah <- sanitize_names(pah)
+  check_valid_chemicals(pah)
+
+  stopifnot(is.numeric(lat) && length(lat) == 1)
+  stopifnot(is.numeric(lon) && length(lon) == 1)
+  stopifnot(is.numeric(depth_m))
+  stopifnot(is.numeric(months) && all(months %in% 1:12))
+
+  attenuation <- list(DOC = DOC, Kd_ref = Kd_ref)
+  attenuation_var <- names(which(vapply(
+    attenuation,
+    Negate(is.null),
+    FUN.VALUE = logical(1)
+  )))
+
+  attenuation_abort <- function() {
+    stop("Either `Kd_ref` or `DOC` must be numeric", call. = FALSE)
+  }
+
+  if (length(attenuation_var) != 1L) {
+    attenuation_abort()
+  }
+  attenuation_vals <- attenuation[[attenuation_var]]
+  if (!is.numeric(attenuation_vals)) {
+    attenuation_abort()
+  }
+
+  elev_m <- elev_m %||% get_elevation(lon, lat)
+  stopifnot(is.numeric(elev_m))
+
+  date <- as.Date(paste0(format(Sys.Date(), "%Y"), "-", months, "-15"))
+
+  grid <- expand.grid(
+    lat = lat,
+    lon = lon,
+    elev_m = elev_m,
+    attenuation = attenuation_vals,
+    depth_m = depth_m,
+    date = date,
+    ...,
+    stringsAsFactors = FALSE
+  )
+
+  df_by_row <- dplyr::rowwise(grid)
+  df_by_row <- dplyr::mutate(
+    df_by_row,
+    tuv_res = list(call_tuv(
+      attenuation = .data$attenuation,
+      attenuation_var = attenuation_var,
+      date = .data$date,
+      lat = .data$lat,
+      lon = .data$lon,
+      elev_m = .data$elev_m,
+      depth_m = .data$depth_m,
+      ...
+    ))
+  )
+
+  df <- dplyr::ungroup(df_by_row)
+  df <- dplyr::mutate(
+    df,
+    pah = pah,
+    nlc50 = nlc50(pah[1]),
+    pabs = vapply(.data$tuv_res, function(x) {
+                        p_abs(x, pah[1])
+                      }, FUN.VALUE = numeric(1)),
+    plc50 = vapply(.data$pabs, function(x) {
+                        plc50(x, pah[1])
+                      }, FUN.VALUE = numeric(1))
+  )
+
+  df
+}
+
+call_tuv <- function(attenuation, attenuation_var, ...) {
+  args <- c(attenuation, list(...))
+
+  names(args)[names(args) == "attenuation"] <- attenuation_var
+
+  do.call("tuv", c(args, quiet = TRUE))
+}
