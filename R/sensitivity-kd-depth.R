afe17a4ff97722e61ed607b7e0c0f6bf8216d516
Andy Teucher <andy.teucher@gmail.com>
2024-02-16 17:09:32 -0800

    Plot for sens_kd_depth()

--- R/sensitivity-kd-depth.R
+++ R/sensitivity-kd-depth.R
@@ -80,11 +80,25 @@ sens_kd_depth <- function(pah = NULL,
     stringsAsFactors = FALSE
   )
 
+  names(grid)[names(grid) == "attenuation"] <- attenuation_var
+
+  n_iter <- nrow(grid)
+
+  if (n_iter > 100) {
+    warning(
+      "Initiating ",
+      n_iter,
+      " iterations of the TUV model. This could take a while",
+      call. = FALSE
+    )
+  }
+
   df_by_row <- dplyr::rowwise(grid)
+
   df_by_row <- dplyr::mutate(
     df_by_row,
     tuv_res = list(call_tuv(
-      attenuation = .data$attenuation,
+      attenuation = .data[[attenuation_var]],
       attenuation_var = attenuation_var,
       date = .data$date,
       lat = .data$lat,
@@ -101,11 +115,11 @@ sens_kd_depth <- function(pah = NULL,
     pah = pah,
     nlc50 = nlc50(pah[1]),
     pabs = vapply(.data$tuv_res, function(x) {
-                        p_abs(x, pah[1])
-                      }, FUN.VALUE = numeric(1)),
+      p_abs(x, pah[1])
+    }, FUN.VALUE = numeric(1)),
     plc50 = vapply(.data$pabs, function(x) {
-                        plc50(x, pah[1])
-                      }, FUN.VALUE = numeric(1))
+      plc50(x, pah[1])
+    }, FUN.VALUE = numeric(1))
   )
 
   df
@@ -117,4 +131,59 @@ call_tuv <- function(attenuation, attenuation_var, ...) {
   names(args)[1] <- attenuation_var
 
   do.call("tuv", c(args, quiet = TRUE))
+}
+
+#' Make a heatmap of the sensitivity analysis performed by [sens_kd_depth()]
+#'
+#' @param x A data.frame, the output of [sens_kd_depth()]
+#'
+#' @return a ggplot2 object
+#' @export
+#'
+#' @examples
+#' out <- sens_kd_depth(
+#'   "Anthracene",
+#'   lat = 52,
+#'   lon = -113,
+#'   Kd_ref = 40:45,
+#'   depth_m = c(0.25, 0.5, 1),
+#'   date = c("2023-07-01", "2023-08-01")
+#' )
+#' plot_sens_kd_depth(out)
+plot_sens_kd_depth <- function(x) {
+  attenuation_var <- intersect(c("DOC", "Kd_ref"), names(x))
+
+  ggplot2::ggplot(x) +
+    ggplot2::geom_tile(
+      ggplot2::aes(
+        x = .data[[attenuation_var]],
+        y = .data$depth_m,
+        fill = .data$plc50
+      )
+    ) +
+    ggplot2::scale_fill_viridis_c(option = "inferno") +
+    ggplot2::scale_x_continuous(
+      breaks = if (length(unique(x[[attenuation_var]])) < 5) {
+        unique(x[[attenuation_var]])
+      } else {
+        ggplot2::waiver()
+      }
+    ) +
+    ggplot2::scale_y_continuous(
+      breaks = if (length(unique(x$depth_m)) < 5) {
+        unique(x$depth_m)
+      } else {
+        ggplot2::waiver()
+      }
+    ) +
+    ggplot2::facet_wrap(ggplot2::vars(.data$date)) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(panel.grid.minor = ggplot2::element_blank()) +
+    ggplot2::labs(
+      title = paste0("PLC50 of ", x$pah[1], " across various depths and values of ",
+                    attenuation_var, ", by date"),
+      x = if (attenuation_var == "DOC") "DOC (mg/L)" else "Kd(ref)",
+      y = "Depth (m)",
+      fill = "PLC50 (ug/L)"
+    )
 }
