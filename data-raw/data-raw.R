ddeadddb38575d706f45dd76e5a62305e837b396
Andy Teucher <andy.teucher@gmail.com>
2024-03-21 16:30:06 -0700

    Add new log_kow and mol_weight for missing chemicals

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,8 +1,10 @@
 devtools::load_all()
 source("data-raw/molar-absorption.R")
 source("data-raw/nlc50.R")
-source("data-raw/o3.R")
-source("data-raw/aerosol-optical-thickness.R")
+
+## Only run these if you've updated them, they take a long time to run
+# source("data-raw/o3.R")
+# source("data-raw/aerosol-optical-thickness.R")
 
 usethis::use_data(
   o3,
